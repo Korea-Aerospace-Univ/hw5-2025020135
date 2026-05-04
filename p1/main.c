8664233a7402baef9f86321da8e4dc3116bbeb6b
2025020135 <ssuhyeon@kau.kr>
2026-05-04 16:00:36 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -6,8 +6,8 @@ int main(void)
     bool found = false;
     scanf("%d",&N);
     for(int i=1;i*900<N; i++) {
-        for(int j=1; j*750<N; j++) {
-            for(int k=2; k*200<N; k+2) {
+        for(int j=2; j*750<N; j+=2) {
+            for(int k=1; k*200<N; k++) {
                 if ((i*900 + j*750 + k*200 == N)&&(k<i || k<j)) {
                     printf("%d %d %d\n",i,j,k);
                     found = true;
