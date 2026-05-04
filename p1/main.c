089493a349d0671bf5e3396b67102f3d72c8b39a
2025020135 <ssuhyeon@kau.kr>
2026-05-04 15:31:44 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,24 +3,21 @@
 int main(void)
 {
     int N;
-    int found = 0;
+    bool found = false;
     scanf("%d",&N);
     for(int i=1;i*900<N; i++) {
         for(int j=1; j*750<N; j++) {
-            for(int k=1; k*200<N; k++) {
-                if (i*900 + j*750 + k*200 == N) {
-                    if (j%2==0) {
-                        if (k<i || k<j){
-                            printf("%d %d %d\n",i,j,k);
-                            found = 1;
-                        }
+            for(int k=2; k*200<N; k+2) {
+                if ((i*900 + j*750 + k*200 == N)&&(k<i || k<j)) {
+                    printf("%d %d %d\n",i,j,k);
+                    found = true;
+                    
                     }
                 }
-                
             }
         }
     }
-    if (found == 0) {
+    if (!found) {
         printf("none");
     }
     return 0;
